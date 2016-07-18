bd1083d9e27d7f04cea6a94efcd3cb1020cc3faa
Teddy Odhiambo <tedb19@gmail.com>
2016-07-18 16:04:34 +0300

    added location details

--- src/main/java/hapimodule/core/entities/Person.java
+++ src/main/java/hapimodule/core/entities/Person.java
@@ -16,6 +16,14 @@ public class Person implements java.io.Serializable {
      private String sex;
      private Date birthdate;
      private Date deathdate;
+     private String villageName;
+     private String locationName;
+     private String subLocationName;
+     private String landMark;
+     private String countyName;
+     private String districtName;
+     private String provinceName;
+     private String division;
      private Set<PersonIdentifier> personIdentifiers;
      private Date dateCreated;
      private Date dateModified;
@@ -53,7 +61,9 @@ public class Person implements java.io.Serializable {
         this.patientSource = patientSource;
     }
     public Person(MaritalStatus maritalStatusType, PatientSource patientSource, String firstName,
-            String middleName, String lastName, String sex, Date birthdate, Date deathdate, Set personIdentifiers) {
+            String middleName, String lastName, String sex, Date birthdate, Date deathdate,
+            String villageName, String locationName, String subLocationName, String landMark, String countyName,
+            String districtName, String provinceName, String division, Set<PersonIdentifier> personIdentifiers) {
        this.maritalStatusType = maritalStatusType;
        this.patientSource = patientSource;
        this.firstName = firstName;
@@ -62,6 +72,14 @@ public class Person implements java.io.Serializable {
        this.sex = sex;
        this.birthdate = birthdate;
        this.deathdate = deathdate;
+       this.countyName = countyName;
+       this.districtName = districtName;
+       this.division = division;
+       this.locationName = locationName;
+       this.provinceName = provinceName;
+       this.subLocationName = subLocationName;
+       this.villageName = villageName;
+       this.landMark = landMark;
        this.personIdentifiers = personIdentifiers;
     }
    
@@ -122,6 +140,70 @@ public class Person implements java.io.Serializable {
     public void setDeathdate(Date deathdate) {
         this.deathdate = deathdate;
     }
+
+	public String getVillageName() {
+		return villageName;
+	}
+
+	public void setVillageName(String villageName) {
+		this.villageName = villageName;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+
+	public String getSubLocationName() {
+		return subLocationName;
+	}
+
+	public void setSubLocationName(String subLocationName) {
+		this.subLocationName = subLocationName;
+	}
+
+	public String getCountyName() {
+		return countyName;
+	}
+
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+
+	public String getLandMark() {
+		return landMark;
+	}
+
+	public void setLandMark(String landMark) {
+		this.landMark = landMark;
+	}
+
+	public String getDistrictName() {
+		return districtName;
+	}
+
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+
+	public void setDivision(String division) {
+		this.division = division;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
 
 }
 
